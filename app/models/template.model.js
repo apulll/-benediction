3af74679c24b9e1dd22baef0a5e68260b4a7673b
capul <1216065136@qq.com>
2018-03-23 14:41:54 +0800

    更新postman

--- app/models/template.model.js
+++ app/models/template.model.js
@@ -2,7 +2,7 @@
 * @Author: perry
 * @Date:   2018-03-14 09:57:50
 * @Last Modified by:   perry
-* @Last Modified time: 2018-03-21 16:39:46
+* @Last Modified time: 2018-03-23 13:47:49
 */
 const Sequelize = require('sequelize');
 const db = require('../db/core.js');
@@ -12,6 +12,9 @@ const Template = db.define('template', {
     type: Sequelize.TEXT
   },
   bg_imgsumb: {
+    type: Sequelize.TEXT
+  },
+  bg_bension_img: {
     type: Sequelize.TEXT
   }
 });
