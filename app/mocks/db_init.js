4e5edf91217b88a4f7fa1d34525b35b6b52e31d2
capul <1216065136@qq.com>
2018-03-24 11:42:49 +0800

    nothing

---
+++ app/mocks/db_init.js
@@ -0,0 +1,34 @@
+/*
+* @Author: perry
+* @Date:   2018-03-24 11:32:53
+* @Last Modified by:   perry
+* @Last Modified time: 2018-03-24 11:35:19
+*/
+export default [
+
+{
+	catalog_name:"朋友",
+	catalog_icon:"",
+	catalog_bg:"",
+	templates:[
+		{
+
+		},
+	]
+},
+{
+	catalog_name:"闺蜜"
+	catalog_icon:""
+	catalog_bg:""
+},
+{
+	catalog_name:"爱情"
+	catalog_icon:""
+	catalog_bg:""
+},
+{
+	catalog_name:"生日"
+	catalog_icon:""
+	catalog_bg:""
+}
+]
