4f4b98f0ccfd77cd9832b00ef4444466de6032e8
capul <1216065136@qq.com>
2018-04-17 22:44:40 +0800

    词库增加

--- app/middlewares/word/index.js
+++ app/middlewares/word/index.js
@@ -2,7 +2,7 @@
 * @Author: perry
 * @Date:   2018-04-17 09:29:54
 * @Last Modified by:   perry
-* @Last Modified time: 2018-04-17 14:13:13
+* @Last Modified time: 2018-04-17 22:44:27
 */
 import wordArray from '../../config/wordFilter.js';
 const KeywordFilter = require('keyword-filter');
@@ -18,13 +18,12 @@ const Logger = require('../../lib/logger')('middlewares/word');
 // 	.toString()
 // 	.split('\n');
 
-// const write_path = path.resolve(__dirname, '../../config/word10.text');
+// const write_path = path.resolve(__dirname, '../../config/word11.text');
 // const write_path_exist = fs.existsSync(write_path);
 // if (write_path_exist) {
 // } else {
 // 	const newArray = JSON.stringify(array);
 // 	fs.writeFile(write_path, newArray);
-
 // }
 // Logger.info(array, 'array112289');
 
