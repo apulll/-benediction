054932064f4d4064c2dbad0e118a449956aed8a8
capul <1216065136@qq.com>
2018-04-17 23:04:31 +0800

    完善词库

--- app/models/index.js
+++ app/models/index.js
@@ -2,23 +2,23 @@
 * @Author: perry
 * @Date:   2018-03-14 15:05:01
 * @Last Modified by:   perry
-* @Last Modified time: 2018-03-28 22:20:32
+* @Last Modified time: 2018-04-17 23:03:44
 */
-import config from "../config";
-const Sequelize = require("sequelize");
-const db = require("../db/core.js");
-const sequelize_fixtures = require("sequelize-fixtures");
-const path = require("path");
-const Promise = require("bluebird");
-const Logger = require("../lib/logger")("model/index");
-import Benison from "./benison.model";
-import Catalog from "./catalog.model";
-import File from "./file.model";
-import Template from "./template.model";
-import User from "./user.model";
-import UserBenison from "./user.benison.model";
-import UserBenisonLike from "./user.benison.like.model";
-import seeders from "../mocks/db_init";
+import config from '../config';
+const Sequelize = require('sequelize');
+const db = require('../db/core.js');
+const sequelize_fixtures = require('sequelize-fixtures');
+const path = require('path');
+const Promise = require('bluebird');
+const Logger = require('../lib/logger')('model/index');
+import Benison from './benison.model';
+import Catalog from './catalog.model';
+import File from './file.model';
+import Template from './template.model';
+import User from './user.model';
+import UserBenison from './user.benison.model';
+import UserBenisonLike from './user.benison.like.model';
+import seeders from '../mocks/db_init';
 
 // UserBenison.sync({force: true})
 Template.belongsTo(Catalog);
@@ -29,29 +29,29 @@ Template.hasMany(Benison);
 User.hasMany(Benison);
 
 const model = {
-	BenisonModel: Benison,
-	CatalogModel: Catalog,
-	TemplateModel: Template,
-	UserModel: User,
-	UserBenisonModel: UserBenison,
-	UserBenisonLikeModel: UserBenisonLike,
-	FileModel: File
+  BenisonModel: Benison,
+  CatalogModel: Catalog,
+  TemplateModel: Template,
+  UserModel: User,
+  UserBenisonModel: UserBenison,
+  UserBenisonLikeModel: UserBenisonLike,
+  FileModel: File
 };
 db.sync().then(aaa => {
-	Logger.info(aaa);
-	sequelize_fixtures
-		.loadFiles(
-			[
-				path.resolve(__dirname, "../seeders/users.js"),
-				path.resolve(__dirname, "../seeders/catalogs.js"),
-				path.resolve(__dirname, "../seeders/templates.js"),
-				path.resolve(__dirname, "../seeders/benisons.js")
-			],
-			model
-		)
-		.then(function(doStuffAfterLoad) {
-			console.log(doStuffAfterLoad, "doStuffAfterLoad");
-		});
+  Logger.info(aaa);
+  sequelize_fixtures
+    .loadFiles(
+      [
+        path.resolve(__dirname, '../seeders/users.js'),
+        path.resolve(__dirname, '../seeders/catalogs.js'),
+        path.resolve(__dirname, '../seeders/templates.js'),
+        path.resolve(__dirname, '../seeders/benisons.js')
+      ],
+      model
+    )
+    .then(function(doStuffAfterLoad) {
+      console.log(doStuffAfterLoad, 'doStuffAfterLoad');
+    });
 });
 
 export default model;
